239d9057fd539738444f43a9d679d0deb38d4cbe
ParsaAbasnezhad <parsaabasy8@gmail.com>
2025-07-04 22:30:39 -0700

    Add Django admin panel

--- Services/admin.py
+++ Services/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 from .models import ServiceCar
-admin.site.register(ServiceCar)
+
+
+
+@admin.register(ServiceCar)
+class ServiceCarAdmin(admin.ModelAdmin):
+    list_display = ('name','price')
+    search_fields = ('name',)
+    list_filter = ('status',)
+
+
+
+
--- Services/models.py
+++ Services/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class ServiceCar(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='services')
-    year = models.DateField()
+    city = models.CharField(max_length=100)
     price = models.IntegerField()
+    year = models.DateField()
     link = models.URLField()
-    def __str__(self):
-        return self.name + ' ' + str(self.price)
+    status = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'ماشین'
+        verbose_name_plural = 'ماشین ها'
